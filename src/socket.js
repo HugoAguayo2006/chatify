9e8bc5dab20a24364508de26a9db51088b689042
HugoAguayo2006 <hugoaguayo50@gmail.com>
2026-04-20 20:11:04 -0600

    Dejamos en el main solo la ui

---
+++ src/socket.js
@@ -0,0 +1,13 @@
+import { io } from 'socket.io-client';
+
+// "undefined" means the URL will be computed from the `window.location` object
+const URL = import.meta.env.VITE_SOCKET_URL || 'http://localhost:3000';
+
+export const socket = io(URL, {
+    auth: {
+      serverOffset: 0,
+      // enable retries
+      ackTimeout: 10000,
+      retries: 3,
+    }
+});
